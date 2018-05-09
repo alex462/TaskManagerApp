a7bdca9680d9911710006a0a50925a5a4fa9b2cd
alex462 <alex.reinhart462@gmail.com>
2018-05-09 10:35:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/alexandrareinhart/taskmanagerapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alexandrareinhart.taskmanagerapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_view_all:
+                    mTextMessage.setText(R.string.title_view_all);
+                    return true;
+                case R.id.navigation_view_incomplete:
+                    mTextMessage.setText(R.string.title_view_incomplete);
+                    return true;
+                case R.id.navigation_view_completed:
+                    mTextMessage.setText(R.string.title_view_completed);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
