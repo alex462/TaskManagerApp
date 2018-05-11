e76544f83b00324b7d464a22f0b1f4bd7df4b04b
alex462 <alex.reinhart462@gmail.com>
2018-05-11 15:13:40 -0400

    working on DatePicker from editText

--- app/src/main/java/com/example/alexandrareinhart/taskmanagerapp/TaskCreatorFragment.java
+++ app/src/main/java/com/example/alexandrareinhart/taskmanagerapp/TaskCreatorFragment.java
@@ -1,5 +1,8 @@
 package com.example.alexandrareinhart.taskmanagerapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,9 +12,12 @@ import android.text.SpannableStringBuilder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.zip.Inflater;
 
@@ -77,6 +83,54 @@ public class TaskCreatorFragment extends Fragment {
         activityCallback.addClicked();
 
         Toast.makeText(getActivity(), "TASK ADDED SUCCESSFULLY", Toast.LENGTH_LONG).show();
+    }
+
+    private void getDateFromEditText() {
+//        final Calendar c = Calendar.getInstance();
+//        int year = c.get(Calendar.YEAR);
+//        int month = c.get(Calendar.MONTH);
+//        int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public Dialog onCreateDialog(Bundle savedInstanceState) {
+//                // Use the current date as the default date in the picker
+//                final Calendar c = Calendar.getInstance();
+//                int year = c.get(Calendar.YEAR);
+//                int month = c.get(Calendar.MONTH);
+//                int day = c.get(Calendar.DAY_OF_MONTH);
+//
+//                // Create a new instance of DatePickerDialog and return it
+//                return new DatePickerDialog(getActivity(),, year, month, day);
+//                }
+//
+//                public void onDateSet(DatePicker view, int year, int month, int day) {
+//                // Do something with the date chosen by the user
+//                    dateEditText.setText(day + "/" + (month + 1) + "/" + year);
+//                    }
+//        }
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int dayOfMonth, int month, int year) {
+
+                    }
+                }, 0, 0, 0);
+                dateEditText.setText(day + "/" + (month + 1) + "/" + year);
+                datePickerDialog.show();
+            }
+
+        }
+
+        );
     }
 
     public void attachParent(ActivityCallback activityCallback) {
